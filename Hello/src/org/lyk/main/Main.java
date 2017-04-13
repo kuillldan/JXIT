e0ece2c7e400984e4a8ea69151c79221d8a2cc0f
ZBOOK <851816676@qq.com>
2017-04-13 21:48:42 +0800

    class loader

--- Hello/src/org/lyk/main/Main.java
+++ Hello/src/org/lyk/main/Main.java
@@ -1,37 +1,33 @@
 package org.lyk.main;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.sql.*;
 import java.text.*;
 import java.util.*;
+import java.util.Date;
+
+import org.lyk.vo.LYKClassLoader;
 
 import utils.StringUtils;
- 
+
 public class Main
 {
-
 	public static void main(String[] args) throws Exception
-	{   
-		Class cls = Class.forName("org.lyk.vo.Dept");
-		Object obj = cls.newInstance();
+	{
+		//用户自定义的类用扩展加载器
+		//系统类
 		
-		String param = "dname:开发部|loc:北京";
-		String[] values = param.split("\\|");
-		for(String value : values)
-		{
-			String fieldName = value.split(":")[0];
-			String fieldValue = value.split(":")[1];
-			Method setter = cls.getMethod("set" + StringUtils.initCap( fieldName), String.class);
-			setter.invoke(obj, fieldValue);
-		}
 		
-		System.out.println(obj);
+		LYKClassLoader lykClassLoader = new LYKClassLoader();
+		Class dateClass = lykClassLoader.getMyClass("org.lyk.vo.Dept");
+		System.out.println(dateClass.getName());
 		
 		
 		
-		System.out.println("///Main done~~")  ;
+		
+		System.out.println("///Main done~~");
 	}
 }
-    
