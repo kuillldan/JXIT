66e402bb06311bf61a474113306fbffcfa3eb6f5
ZBOOK <851816676@qq.com>
2017-05-24 23:44:15 +0800

    I am tired....

--- temp/Hello.java
+++ temp/Hello.java
@@ -1,7 +1,13 @@
-public class Hello
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello Shit");
+public class Hello {
+    public static void main(String[] args) 
+    { 
+        if (true) 
+        {
+            int i = 10;
+            System.out.println(i);
+        }
+
+        
+        System.out.println( i);
     }
 }
