eda1d064114742fd7ccc26ad31049d0fffe9ee13
'sheldon_745' <'yuan-kuil@hpe.com'>
2017-05-29 18:01:57 +0800

    training is boring.

--- HibernateProject528/src/lyk/main/Hello.java
+++ HibernateProject528/src/lyk/main/Hello.java
@@ -1,61 +1,42 @@
 package lyk.main;
 
 import java.util.ArrayList;
-import java.util.List; 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
- 
+import java.util.UUID;
 
-
-
-
-
-
+import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 import dbc.HibernateSessionFactory;
-import lyk.vo.Groups;
-import lyk.vo.Item; 
+import lyk.vo.Item;
 import lyk.vo.MemberDetails;
 import lyk.vo.MemberLogin;
 import lyk.vo.News;
-import lyk.vo.Role;
-import lyk.vo.Subitem; 
+import lyk.vo.Subitem;
+
 public class Hello
-{
-	public static List<News> allNews = new ArrayList<News>();
+{ 
 	public static void main(String[] args) throws InterruptedException
 	{
-		Session hibernateSession = HibernateSessionFactory.getSession();
-//		 
-//		MemberLogin login = new MemberLogin();
-//		login.setMid("sheldon-complete");
-//		login.setPassword("123");
-//		
-//		MemberDetails details = new MemberDetails();
-//		details.setMid("sheldon-complete");
-//		details.setName("yuankui");
-//		details.setPhone("132123fffff");
-//		details.setEmail("kuikui@hpe.com");
-//		
-//		login.setMemberDetails(details);
-//		
-//		hibernateSession.update(login); 
-//		hibernateSession.beginTransaction().commit();
+		List<String> allMsg = new ArrayList<String>();
 		
-		
-//		MemberLogin login = (MemberLogin) hibernateSession.get(MemberLogin.class, "sheldon-complete");
-//		System.out.println(login);
-		
-		String hsql = " FROM MemberLogin WHERE mid=? ";
-		Query query = hibernateSession.createQuery(hsql);
-		query.setParameter(0, "sheldon-complete");
-		MemberLogin login = (MemberLogin) query.uniqueResult();
-		System.out.println(login);
-		System.out.println(login.getMemberDetails());
+
+		for (Object msg : allMsg)
+		{
+			System.out.println("[类型:" + msg.getClass().getSimpleName() + ",值:" + msg + "]");
+		}
+
 		System.out.println("//Main done~~~");
 	}
+
+	public static void change(List allObjs)
+	{
+		allObjs.set(1, 3232);
+	}
 }
-
-
