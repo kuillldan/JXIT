fbbb425113e42c92e046b5e1688affeae8757895
sheldon_745 <yuan-kuil@hpe.com>
2017-08-02 09:48:04 +0800

    daily check in

--- BITool/src/bitool/enums/OpenOffStatus.java
+++
@@ -1,6 +0,0 @@
-package bitool.enums;
-
-public enum OpenOffStatus
-{
-	OPEN,CLOSED,ADMIN_OPEN
-}
--- BITool/src/bitool/service/IAccountManagementService.java
+++
@@ -1,8 +0,0 @@
-package bitool.service;
-
-import bitool.vo.AccountManagement;
-
-public interface IAccountManagementService
-{
-	public AccountManagement findAccountByIpAddress(String ipaddress) throws Exception;
-}
