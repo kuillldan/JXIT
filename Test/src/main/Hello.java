fdab28e0a95241f9e035ade12e9dbc32f24404cd
sheldon_745 <yuan-kuil@hpe.com>
2017-08-05 14:25:54 +0800

    daily checkin by bat

--- Test/src/main/Hello.java
+++ Test/src/main/Hello.java
@@ -7,14 +7,33 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 import dbc.DatabaseConnection;
- 
+
+enum Color
+{
+	RED(1),BLUE(2),YELLOW(3),BLACK(4);
+	
+	private Integer value;
+	private Color(Integer value)
+	{
+		this.value = value;
+	}
+	
+	public Integer getValue()
+	{
+		return value;
+	}
+}
+
 public class Hello
 {
 	public static void main(String[] args) throws Exception
 	{
-		DatabaseConnection dbc = new DatabaseConnection();
-		System.out.println(dbc.getConnection());
-		dbc.close();
+		func("aa","bb","cc");
 		System.out.println("//main done");
+	}
+	
+	public static void func(String ... strs)
+	{
+		System.out.println(strs.length);
 	}
 }
