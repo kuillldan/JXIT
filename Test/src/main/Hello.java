7863cc34b62b795a35b507abc45f6508f5037b56
ZBOOK <851816676@qq.com>
2017-09-18 11:00:03 +0800

    commit by job

--- Test/src/main/Hello.java
+++ Test/src/main/Hello.java
@@ -3,25 +3,19 @@ package main;
 import java.util.Arrays;
 
 import equals.Book;
-import equals.Employee;
-import utils.ObjectAnalyzer;
+import equals.Employee; 
+
+class A{}
+class B extends A{}
+class C extends B{}
+
 
 public class Hello
 {
 	public static void main(String[] args)
 	{
-		Employee e = new Employee();
-		Book b = new Book();
-		b.setName("JAVA核心技术");
-		b.setOwner(e);
-		
-		e.setName("yuankui");
-		e.setSalary(99.3);
-		e.setBook(b);
-		
-		Object[] all = new Object[5];
-		all[0] = 1;
-		all[1] = "abc";
-		System.out.println(all.getClass().getComponentType());
+		A[] all = new C[2];
+		B[] allB = (B[])all;
+		System.out.println(allB.getClass().getComponentType());
 	}
 }
---
+++ temp/Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		Object[] all = new Object[2];
+		String[] strAll = (String[])all;
+		System.out.println(strAll);
+	}
+}
