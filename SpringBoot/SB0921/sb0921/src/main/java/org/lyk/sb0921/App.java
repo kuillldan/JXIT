8fa0b4fcba42ff086b3fa15daabe8cd50977b0ae
ZBOOK <851816676@qq.com>
2017-09-21 23:00:05 +0800

    daily check in

--- SpringBoot/SB0921/sb0921/src/main/java/org/lyk/sb0921/App.java
+++ SpringBoot/SB0921/sb0921/src/main/java/org/lyk/sb0921/App.java
@@ -6,8 +6,8 @@ package org.lyk.sb0921;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
 }
