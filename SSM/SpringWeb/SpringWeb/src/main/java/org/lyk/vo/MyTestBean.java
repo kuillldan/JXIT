c219414726b6fd560d2d64561971caeeffca1454
ZBOOK <851816676@qq.com>
2017-10-09 17:00:01 +0800

    commit by job

---
+++ SSM/SpringWeb/SpringWeb/src/main/java/org/lyk/vo/MyTestBean.java
@@ -0,0 +1,16 @@
+package org.lyk.vo;
+
+public class MyTestBean
+{
+	private String testStr = "testStr";
+	
+	public String getTestStr()
+	{
+		return testStr;
+	}
+	
+	public void setTestStr(String testStr)
+	{
+		this.testStr = testStr;
+	}
+}
