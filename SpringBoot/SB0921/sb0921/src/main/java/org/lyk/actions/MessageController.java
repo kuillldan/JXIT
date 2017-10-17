8272df53bcec34f38d9dddcb1521175953b00da6
ZBOOK <851816676@qq.com>
2017-10-17 17:47:35 +0800

    daily check in

--- SpringBoot/SB0921/sb0921/src/main/java/org/lyk/actions/MessageController.java
+++ SpringBoot/SB0921/sb0921/src/main/java/org/lyk/actions/MessageController.java
@@ -3,6 +3,7 @@ package org.lyk.actions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/message/*")
@@ -14,5 +15,12 @@ public class MessageController
 		model.addAttribute("url", "www.t66y.com");
 		model.addAttribute("mid", mid);
 		return "message/message_show";
+	}
+	
+	@ResponseBody
+	@RequestMapping("getInfo")
+	public String getInfo()
+	{
+		return "这个好像不管用";
 	}
 }
