bcd8243ee3da9f38397db9bccad380c6b7783bd4
ZBOOK <851816676@qq.com>
2017-10-20 12:30:11 +0800

    daily check in

--- SpringBoot/SB0921/sb0921/src/main/java/org/lyk/actions/MemberController.java
+++ SpringBoot/SB0921/sb0921/src/main/java/org/lyk/actions/MemberController.java
@@ -1,10 +1,17 @@
 package org.lyk.actions;
 
-import java.lang.reflect.Member;
 
+
+import java.util.List;
+
+import org.lyk.vo.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import ch.qos.logback.core.joran.conditional.ElseAction;
 
 @Controller
 public class MemberController extends AbstractController
@@ -17,8 +24,19 @@ public class MemberController extends AbstractController
 	
 	@RequestMapping("/add")
 	@ResponseBody
-	public Object add(Member vo)
+	public Object add(Member vo,BindingResult bindingResult)
 	{
+		if(bindingResult.hasErrors())
+		{
+			System.out.println("member数据接收发生异常");
+			List<ObjectError> allErrors = bindingResult.getAllErrors();
+			for(ObjectError oe : allErrors)
+			{
+				System.out.println(oe.getCode() + ":" + oe.getDefaultMessage());
+			}
+			
+			return allErrors;
+		}
 		return vo;
 	}
 }
--- SpringBoot/SB0921/sb0921/src/main/java/org/lyk/vo/Member.java
+++ SpringBoot/SB0921/sb0921/src/main/java/org/lyk/vo/Member.java
@@ -3,11 +3,24 @@ package org.lyk.vo;
 import java.io.Serializable;
 import java.sql.Date;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
 public class Member implements Serializable
 {
-	private String mid;
+	@NotNull(message="{meggmber.mid.notnull.error}")
+	@Email(message="{member.mid.email.error}")
+	private String mid; 
+	@NotNull(message="{member.age.notnull.error}")
+	@Digits(integer=3,fraction=0,message="{member.age.digits.error}")
 	private Integer age;
+	@NotNull(message="{member.salary.notnull.error}")
+	@Digits(integer=10,fraction=2,message="{member.salary.digits.error}")
 	private Double salary;
+	@NotNull(message="{member.birthday.notnull.error}")
 	private Date birthday;
 
 	public String getMid()
